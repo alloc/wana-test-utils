cacc324a519ebf5374cbbc3c33a3acfefc0133ed
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-06-08 19:22:33 -0400

    fix: let globals be injected
    Otherwise, the `wana` package cannot use this package for its own tests, because the compiled `wana` will be imported, even though the tests use the source files directly.

--- src/setupEnv.ts
+++ src/setupEnv.ts
@@ -1,9 +1,12 @@
-import { globals } from 'wana'
+import { globals as G } from 'wana'
 
 export interface EnvConfig {
   act: (effect: () => any) => any
+  globals?: {
+    batchedUpdates: typeof G.batchedUpdates
+  }
 }
 
-export const setupEnv = ({ act }: EnvConfig) => {
+export const setupEnv = ({ act, globals = G }: EnvConfig) => {
   globals.batchedUpdates = effect => void act(effect)
 }
